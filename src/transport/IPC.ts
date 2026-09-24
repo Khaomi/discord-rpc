0c35ac74b7994b6018f11a2c33bf6abcf1a83827
Khaomi <git@khaomi.dev>
2026-09-24 09:53:39 +0700

    feat: cleanup before calling connect

--- src/transport/IPC.ts
+++ src/transport/IPC.ts
@@ -158,6 +158,7 @@ export class IPCTransport extends Transport {
     }
 
     public async connect(): Promise<void> {
+        if (this.isConnected) await this.close();
         if (!this.socket) this.socket = await this.getSocket();
 
         this.emit("open");
