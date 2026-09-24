20d9ab8e37ceaf8a1e36643ebdc0a0e4bf0ce97c
Khaomi <git@khaomi.dev>
2026-09-24 09:16:04 +0700

    feat: add reconnecting example

---
+++ examples/simple_reconnect.ts
@@ -0,0 +1,36 @@
+import { Client, type SetActivity } from "@xhayper/discord-rpc";
+
+const client = new Client({
+    clientId: "123456789012345678"
+});
+
+let lastActivity: SetActivity | undefined = {
+    state: "Suffering with my life",
+    details: "Pain and Suffering",
+    startTimestamp: Date.now(),
+    largeImageKey: "main",
+    largeImageText: "me irl"
+};
+
+const setActivity = async (activity: SetActivity) => {
+    lastActivity = activity;
+    await client.user?.setActivity(activity).then(() => {
+        lastActivity = undefined;
+    });
+}
+
+client.on("ready", async () => {
+    if (lastActivity)
+        await setActivity(lastActivity);
+});
+
+client.on("disconnected", async () => {
+    const interval = setInterval(async () => {
+        if (client.isConnected)
+            clearInterval(interval);
+
+        await client.connect();
+    }, 5_000);
+})
+
+client.login();
